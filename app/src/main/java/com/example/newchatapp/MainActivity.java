a5850e51d60fd09f49ec8a20cd30b3cf79c74564
Aryan Soni <web.vault0516@gmail.com>
2026-04-28 01:07:21 +0530

    Initial Android app setup with Firebase integration

---
+++ app/src/main/java/com/example/newchatapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.newchatapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        Toast.makeText(this,
+                "Firebase Connected Successfully",
+                Toast.LENGTH_LONG).show();
+    }
+}
